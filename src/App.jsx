ca80fb4ffbf24f460a5a261d209ebcaaa1b550dd
Prankul <vprankul@gmail.com>
2023-02-25 00:28:43 +0530

    Added components for search bar and made a css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SearchBar from "./components/Search-Bar";
+
+function App() {
+  return (
+    <div>
+      <h1>This is a search bar</h1>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search-Bar.jsx
@@ -0,0 +1,11 @@
+import "./Search-Bar.css";
+
+function SearchBar() {
+  return (
+    <div>
+      <input className="search-input" />
+    </div>
+  );
+}
+
+export default SearchBar;
